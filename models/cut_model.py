49643dd15ffd174dc69eb4f0a5ae4d849fa1a4f5
Usama1238 <68499486+Usama1238@users.noreply.github.com>
2023-01-24 12:45:42 +0500

    Update cut_model.py

--- models/cut_model.py
+++ models/cut_model.py
@@ -23,7 +23,7 @@ class CUTModel(BaseModel):
 
         parser.add_argument('--lambda_GAN', type=float, default=1.0, help='weight for GAN loss：GAN(G(X))')
         parser.add_argument('--lambda_NCE', type=float, default=1.0, help='weight for NCE loss: NCE(G(X), X)')
-        parser.add_argument('--lambda_idt',type=float,default=10.0,help='weight for idt loss
+        parser.add_argument('--lambda_idt',type=float,default=10.0,help='weight for idt loss')
         parser.add_argument('--nce_idt', type=util.str2bool, nargs='?', const=True, default=False, help='use NCE loss for identity mapping: NCE(G(Y), Y))')
         parser.add_argument('--nce_layers', type=str, default='0,4,8,12,16', help='compute NCE loss on which layers')
         parser.add_argument('--nce_includes_all_negatives_from_minibatch',
